05ba877519d8b45e85bd0b266a07993f95838080
Leonardo D. Palma <lpalmaizaguirre@gmail.com>
2023-05-16 08:58:12 -0600

    Fix Color Theme

--- AlertDialog/src/main/java/com/github/leodan11/alertdialog/dist/helpers/AlertDialog.kt
+++ AlertDialog/src/main/java/com/github/leodan11/alertdialog/dist/helpers/AlertDialog.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.annotation.AttrRes
 import androidx.core.widget.addTextChangedListener
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.google.android.material.textfield.TextInputEditText
@@ -33,39 +34,279 @@ object AlertDialog {
     const val INPUT_TYPE_PERCENTAGE: Int = 9433
     const val INPUT_TYPE_DECIMAL_NUMBER: Int = 5234
 
+    /**
+     * Get Background Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
     fun getColorDefaultBackgroundTheme(context: Context): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(com.google.android.material.R.attr.backgroundColor, typedValue, true)
         return typedValue.data
     }
 
+    /**
+     * Get OnBackground Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnBackgroundTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnBackground, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get theme color by resource id.
+     *
+     * @param context the parent context
+     * @param idAttrRes the id attr resource, e.g. [android.R.attr.colorAccent]
+     */
+    fun getColorDefaultCustomResourceTheme(context: Context, @AttrRes idAttrRes: Int): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(idAttrRes, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get Error Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
     fun getColorDefaultErrorTheme(context: Context): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(com.google.android.material.R.attr.colorError, typedValue, true)
         return typedValue.data
     }
 
-    fun getColorDefaultNeutralTheme(context: Context): Int {
+    /**
+     * Get OnError Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnErrorTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnError, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get ErrorContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultErrorContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorErrorContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnErrorContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnErrorContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnErrorContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get Outline Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOutlineTheme(context: Context): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(com.google.android.material.R.attr.colorOutline, typedValue, true)
         return typedValue.data
     }
 
-    fun getColorDefaultOnSurfaceTheme(context: Context): Int {
+    /**
+     * Get OutlineVariant Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOutlineVariantTheme(context: Context): Int {
         val typedValue = TypedValue()
-        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSurface, typedValue, true)
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOutlineVariant, typedValue, true)
         return typedValue.data
     }
 
+    /**
+     * Get Primary Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
     fun getColorDefaultPrimaryTheme(context: Context): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(com.google.android.material.R.attr.colorPrimary, typedValue, true)
         return typedValue.data
     }
 
+    /**
+     * Get OnPrimary Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnPrimaryTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnPrimary, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get PrimaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultPrimaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorPrimaryContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnPrimaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnPrimaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnPrimaryContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get Secondary Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultSecondaryTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorSecondary, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnSecondary Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnSecondaryTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSecondary, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get SecondaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultSecondaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorSecondaryContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnSecondaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnSecondaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSecondaryContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get Surface Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
     fun getColorDefaultSurfaceTheme(context: Context): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(com.google.android.material.R.attr.colorSurface, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnSurface Color Default Theme Material Design
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnSurfaceTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSurface, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get SurfaceVariant Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultSurfaceVariantTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorSurfaceVariant, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnSurfaceVariant Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnSurfaceVariantTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSurfaceVariant, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get Tertiary Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultTertiaryTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorTertiary, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnTertiary Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnTertiaryTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnTertiary, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get TertiaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultTertiaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorTertiaryContainer, typedValue, true)
+        return typedValue.data
+    }
+
+    /**
+     * Get OnTertiaryContainer Color Only Theme Material Design 3
+     *
+     * @param context the parent context
+     */
+    fun getColorDefaultOnTertiaryContainerTheme(context: Context): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnTertiaryContainer, typedValue, true)
         return typedValue.data
     }
 
